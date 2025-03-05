4f1663869e5a52ab07cbb0d699b97c9a1a09fcbf
ricknullsec <177259409+ricknullsec@users.noreply.github.com>
2025-03-04 16:57:38 -0800

    Added error checking when request isnt 200 from my_status

--- basic-client.py
+++ basic-client.py
@@ -26,7 +26,10 @@ def my_status():
     headers['Authorization'] = 'Bearer ' + API_KEY
     api_url = url + '/my/details'
     response = requests.get(api_url, headers=headers)
+    if response.status_code != 200:
+        return "Error: " + response.text
     response_json = response.json()
+    print(response)
     return "You are playing as " + response_json['data']['username'] + " and you are " + ("banned." if response_json['data']['banned'] else "not banned.")
 
 
