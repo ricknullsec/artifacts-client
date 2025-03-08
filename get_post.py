986917a616cfdac9567197cc8f8627ee1ecc1941
ricknullsec <177259409+ricknullsec@users.noreply.github.com>
2025-03-08 13:57:22 -0800

    Fixed by adding errocode checking and retry for get and post requests

--- get_post.py
+++ get_post.py
@@ -5,23 +5,34 @@ import re
 #Function to make a get request to the API
 def get_request(headers, api_url):
     response = requests.get(api_url, headers=headers)
+    response_json = response.json()
     if response.status_code == 499:
-        error_message = response.get("error", {}).get("message", "")
+        error_message = response_json['error']['message']
         if "cooldown" in error_message:
             error_parse = re.search(r"(\d+\.?\d*) seconds left", error_message)
             if error_parse:
                 print("Waiting on cooldown of {error_parse.group(1)}")
                 time.sleep(float(error_parse.group(1)))
-                response = requests.get(api_url, headers=headers)
+                return get_request(headers, api_url)
     elif response.status_code != 200:
         raise Exception("Error: " + response.text)
-    response_json = response.json()
-    return response_json
+    else:
+        return response_json
 
 #This function takes in the headers and the api_url
 def post_request(headers, api_url, data = None):
     response = requests.post(api_url, headers=headers, json=data)
-    if response.status_code != 200:
-        raise Exception("Error: " + response.text)
     response_json = response.json()
-    return response_json
+    if response.status_code == 499:
+        error_message = response_json['error']['message']
+        if "cooldown" in error_message:
+            error_parse = re.search(r"(\d+\.?\d*) seconds left", error_message)
+            if error_parse:
+                error_time = error_parse.group(1)
+                print(f"Waiting on cooldown of {error_time}")
+                time.sleep(float(error_parse.group(1)))
+                return post_request(headers, api_url, data)
+    elif response.status_code != 200:
+        raise Exception("Error: " + response.text)
+    else:
+        return response_json
